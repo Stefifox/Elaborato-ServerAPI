f45d1fcc60d3dd2e86c8ef095eadcc4549bf3391
Stefano <30950154+Stefifox@users.noreply.github.com>
2021-04-29 16:13:11 +0200

    Correzione sintassi del codice

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ const port = 5500;
 
 //Configurazione app
 app.use(cors())
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(bodyParser.json());
 
 let con = mysql.createConnection({
@@ -37,38 +39,56 @@ app.get('/users', (req, res) => {
     response["content"] = "List of users"
     response["users"] = []
 
-    con.query(`SELECT * FROM utenti ORDER BY utenti.idUtente`, (err, ris)=>{
-        if(err){
-            res.status(500).json({"response":500,"description": err})
-            throw err
-        }
-        ris.forEach(element => {
-            //console.log(element)
-            let data = new Object()
-            data["id"] = element.idUtente
-            data["username"] = element.user
-            data["password"] = element.pass
-            response["users"].push(data)
-        });
-        res.status(200).json(response)
-    }) 
+    try {
+        con.query(`SELECT * FROM utenti ORDER BY utenti.idUtente`, (err, ris) => {
+            if (err) {
+                res.status(500).json({
+                    "response": 500,
+                    "description": err
+                })
+            }
+            ris.forEach(element => {
+                //console.log(element)
+                let data = new Object()
+                data["id"] = element.idUtente
+                data["username"] = element.user
+                data["password"] = element.pass
+                response["users"].push(data)
+            });
+            res.status(200).json(response)
+        })
+    } catch (error) {
+        console.log(error)
+    }
+
 });
 
 //.post Definisce cosa deve fare il server in caso di richiesta post
 // /resgisttation può essere chiamato mediante metodo post e si occupa di inserire un nuovo utente nel database
-app.post('/registration', (req, res)=>{
+app.post('/registration', (req, res) => {
     let data = req.body
     let user = data.username
     let pass = data.password
 
-    con.query(`INSERT INTO utenti (user, pass) VALUES (?,?)`,[user, pass], err=>{
+    try {
+        con.query(`INSERT INTO utenti (user, pass) VALUES (?,?)`, [user, pass], err => {
 
-        if(err){
-            res.status(500).json({"response":500,"description": err})
-            throw err
-        }
-        res.status(200).json({"response":200,"description": "Ok"})
-    })    
+            if (err) {
+                res.status(500).json({
+                    "response": 500,
+                    "description": err
+                })
+
+            }
+            res.status(200).json({
+                "response": 200,
+                "description": "Ok"
+            })
+        })
+    } catch (error) {
+        console.log(error)
+    }
+
 })
 
 //.listen Avvia il server sulla porta specificata e rimane in ascolto per le richieste
